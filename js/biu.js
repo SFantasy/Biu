3a97179cdadafe7cf3006bc55de6b715ef5171b5
SFantasy <forever.fantasy27@gmail.com>
2013-04-19 19:05:50 +0800

    Add 'more-items'

---
+++ js/biu.js
@@ -0,0 +1,8 @@
+// show hidden items
+function showItems () {
+	if($('.more-items').css('display') == 'block') {
+		$('.more-items').css('display', 'none');
+	}  else {
+		$('.more-items').css('display', 'block');
+	}
+}
