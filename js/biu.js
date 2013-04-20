51c4f4bcb47d22cbbf9ce5b7a4e7ca9a8322cd31
SFantasy <forever.fantasy27@gmail.com>
2013-04-20 15:01:41 +0800

    Add two kinds of forms

--- js/biu.js
+++ js/biu.js
@@ -1,8 +1,21 @@
+/*
+ *   Require jquery.js v1.9.1
+*/
+
 // show hidden items
-function showItems () {
+$('.show-more').bind('click', function() {
 	if($('.more-items').css('display') == 'block') {
 		$('.more-items').css('display', 'none');
-	}  else {
+	} else {
 		$('.more-items').css('display', 'block');
 	}
-}
+});
+
+// textarea form event
+$('.form-area textarea').focus(function() {
+	$(this).css('height', '50px');
+});
+
+$('.form-area textarea').focusout(function() {
+	$(this).css('height', '16px');
+});
