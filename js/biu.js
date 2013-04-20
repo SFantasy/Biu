012ae878aeceeb317990a12f49b759a8fc1c1529
SFantasy <forever.fantasy27@gmail.com>
2013-04-20 15:02:59 +0800

    Merge from gh-pages

--- js/biu.js
+++ js/biu.js
@@ -1,3 +1,21 @@
-(function () {
-	
-} ) (this);
+/*
+ *   Require jquery.js v1.9.1
+*/
+
+// show hidden items
+$('.show-more').bind('click', function() {
+	if($('.more-items').css('display') == 'block') {
+		$('.more-items').css('display', 'none');
+	} else {
+		$('.more-items').css('display', 'block');
+	}
+});
+
+// textarea form event
+$('.form-area textarea').focus(function() {
+	$(this).css('height', '50px');
+});
+
+$('.form-area textarea').focusout(function() {
+	$(this).css('height', '16px');
+});
